01da81252751241ce2cac8cc17ee6c2082a6130b
menaaziz27 <menaaziz27@gmail.com>
2020-08-20 09:12:36 +0200

    added class based views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 
 def home(request):
     context = {
@@ -22,5 +17,49 @@ def home(request):
     }
     return render(request, "blog/home.html", context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html" #<app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post 
+    #blog/post_detail.html
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post 
+    fields = ["title", "content"]
+
+    ''' every post has an author and the author by default is NULL
+        so we need to override form_valid function to make the author
+        the same person currently logged in 
+        so it allows us to add the author before the form is submitted'''
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post 
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object() #to get the post that we actually want to update
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post 
+    success_url = reverse_lazy("home")
+    def test_func(self):
+        post = self.get_object() #to get the post that we actually want to update
+        if self.request.user == post.author:
+            return True
+        return False
 def about(request):
     return render(request, "blog/about.html", {"title" : "About"})
