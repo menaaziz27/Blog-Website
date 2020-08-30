da4ff091de62053574331151e4dd8ef2459ea1cb
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2020-08-30 02:18:47 +0200

    refactor settings

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,9 @@ from .models import Profile
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
+class Meta:
+    model = User
+    fields = ["username", "email", "password1", "password2"]
 
 
 class UserUpdateForm(forms.ModelForm):
